0e0853f35f5caacc170fbbc5273305247ee156cd
Titus <titus@gahissy.com>
2025-02-21 16:23:50 +0000

    Forms: validation and state

--- src/06/Form02.tsx
+++ src/06/Form02.tsx
@@ -1,67 +1,101 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {RiLoader5Line} from "react-icons/ri";
+import {BiError} from "react-icons/bi";
+
+
+interface ContactForm {
+  to: string
+  subject: string
+  message: string
+}
 
 
 export default function Form02() {
 
-    const [to, setTo] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
+    const [form, setForm] = useState<ContactForm>({
+      to: "",
+      subject: "",
+      message: ""
+    });
+    const [submitting, setSubmitting] = useState(false);
+    const [error, setError] = useState("");
 
     function resetForm() {
-        setTo("");
-        setSubject("");
-        setMessage("");
+        setForm({
+          to: "",
+          subject: "",
+          message: ""
+        })
+        setSubmitting(false);
+        setError("");
     }
 
-    function sendMessage() {
-        alert("Message envoyé à : " + to);
-        resetForm();
+    function sendMessage(event: FormEvent<HTMLFormElement>) {
+      // stop l'envoi du formulaire au backend (action)
+      event.preventDefault();
+      // Valider le formulaire
+      // Envoyer au backend
+      // Gérer les messages d'erreur
+
+      if (form.subject.length < 10) {
+        setError("Le sujet doit comporter au moins 10 caractères.");
+      } else {
+        setError("");
+        setSubmitting(true);
+        setTimeout(resetForm, 5000)
+      }
     }
 
     function handleChangeTo(event: ChangeEvent<HTMLInputElement>) {
-        setTo(event.target.value)
+        setForm({
+          ...form,
+          to: event.target.value,
+        })
     }
 
     function handleChangeSubject(event: ChangeEvent<HTMLInputElement>) {
-        setSubject(event.target.value)
+      setForm({
+        ...form,
+        subject: event.target.value,
+      })
     }
 
     function handleChangeMessage(event: ChangeEvent<HTMLTextAreaElement>) {
-        setMessage(event.target.value)
+      setForm({
+        ...form,
+        message: event.target.value
+      })
     }
 
     return (
         <div className="p-12 space-y-2">
-
             <form onSubmit={sendMessage}>
                 <div>
                     <label>Destinataire <span className="text-red-500">*</span></label>
-                    <input type="email" value={to}
+                    <input type="email" value={form.to}
                            required
                            onChange={handleChangeTo}
                            className="block border p-1" placeholder="contact@email.com"/>
                 </div>
-
                 <div>
                     <label>Sujet du message <span className="text-red-500">*</span></label>
-                    <input type="text" value={subject}
+                    <input type="text" value={form.subject}
                            required
                            onChange={handleChangeSubject}
                            className="block border p-1" placeholder="Sujet"/>
                 </div>
-
                 <div>
                     <label>Message <span className="text-red-500">*</span></label>
-                    <textarea value={message}
+                    <textarea value={form.message}
                               required
                               onChange={handleChangeMessage}
                               className="block border p-1" placeholder="Le contenu de votre message"/>
                 </div>
-
                 <div className="pt-2 space-x-2">
                     <button type="submit"
+                            disabled={submitting}
                             className="bg-black text-white uppercase text-xs tracking-wide px-4 py-1">
-                        Envoyer
+                      {submitting ?<RiLoader5Line className="animate-spin" /> : "Envoyer" }
                     </button>
                     <button className="text-gray-600"
                             onClick={resetForm}>Effacer
@@ -69,6 +103,11 @@ export default function Form02() {
                 </div>
             </form>
 
+          {error && (
+            <div className="bg-red-50  p-3 text-red-500 text-xs flex items-center space-x-2">
+            <BiError />
+            <span>Le formulaire comporte des erreurs.</span>
+          </div>)}
         </div>
     )
 }
